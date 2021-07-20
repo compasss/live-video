46621ea860ec7b4139d8df59859081c39e3597a7
compasss <crazymingzai@gmail.com>
2021-07-20 15:08:36 +0800

    change real-url sdk

--- vdl/realurl/huya.py
+++ vdl/realurl/huya.py
@@ -7,6 +7,7 @@ import base64
 import urllib.parse
 import hashlib
 import time
+import json
 
 
 class HuYa:
@@ -23,47 +24,16 @@ class HuYa:
                               '(KHTML, like Gecko) Chrome/75.0.3770.100 Mobile Safari/537.36 '
             }
             response = requests.get(url=room_url, headers=header).text
-            livelineurl = re.findall(r'liveLineUrl = "([\s\S]*?)";', response)[0]
-            livelineurl = base64.b64decode(livelineurl).decode('utf-8')
-            if livelineurl:
-                if 'replay' in livelineurl:
-                    real_url = {
-                        'replay': "https:" + livelineurl,
-                    }
-                else:
-                    s_url = self.live(livelineurl)
-                    b_url = self.live(livelineurl.replace('_2000', ''))
-                    real_url = {
-                        '2000p': "https:" + s_url,
-                        'tx': "https:" + b_url,
-                        'bd': "https:" + b_url.replace('tx.hls.huya.com', 'bd.hls.huya.com'),
-                        'migu-bd': "https:" + b_url.replace('tx.hls.huya.com', 'migu-bd.hls.huya.com'),
-                    }
-            else:
+            streamInfo = json.loads(re.findall(r"<script> window.HNF_GLOBAL_INIT = (.*)</script>", response)[0])["roomInfo"]["tLiveInfo"]["tLiveStreamInfo"]["vStreamInfo"]["value"]
+            if streamInfo == []:
                 raise Exception('未开播或直播间不存在')
+            real_url = {}
+            for info in streamInfo:
+                real_url[info["sCdnType"].lower() + "_flv"] = info["sFlvUrl"] + "/" + info["sStreamName"] + "." + info["sFlvUrlSuffix"] + "?" + info["sFlvAntiCode"]
+                real_url[info["sCdnType"].lower() + "_hls"] = info["sHlsUrl"] + "/" + info["sStreamName"] + "." + info["sHlsUrlSuffix"] + "?" + info["sHlsAntiCode"]
         except Exception as e:
             raise Exception('未开播或直播间不存在')
         return real_url
-
-    @staticmethod
-    def live(e):
-        i, b = e.split('?')
-        r = i.split('/')
-        s = re.sub(r'.(flv|m3u8)', '', r[-1])
-        c = b.split('&', 3)
-        c = [i for i in c if i != '']
-        n = {i.split('=')[0]: i.split('=')[1] for i in c}
-        fm = urllib.parse.unquote(n['fm'])
-        u = base64.b64decode(fm).decode('utf-8')
-        p = u.split('_')[0]
-        f = str(int(time.time() * 1e7))
-        ll = n['wsTime']
-        t = '0'
-        h = '_'.join([p, t, s, f, ll])
-        m = hashlib.md5(h.encode('utf-8')).hexdigest()
-        y = c[-1]
-        url = "{}?wsSecret={}&wsTime={}&u={}&seqid={}&{}".format(i, m, ll, t, f, y)
-        return url
 
 
 def get_real_url(rid):
