175d0dc8d52478a4605c146d74a1cf2a1c68e7c5
compasss <crazymingzai@gmail.com>
2022-07-07 15:26:26 +0800

    change config

--- video_download/settings.py
+++ video_download/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = '!ij5w55$e@%5t&^k#n6usu$oy1jw3+u&=wpzkbmt5_o)rcji@#'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['10.0.13.142']
 
 
 # Application definition
